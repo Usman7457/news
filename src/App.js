8841995259f0f9a6ba16942bb0bea94dfb62d193
Usman7457 <usman.nxg@gmail.com>
2023-11-14 19:19:18 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ const [progress, setProgress] = useState(0)
         progress={progress}
       />
         <Routes>
-        <Route path="/" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" />} />
-        <Route path="/business" element={<News setProgress={setProgress} key="business" pageSize={pageSize} country="us" category="business" />} />
-        <Route path="/entertainment" element={<News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="us" category="entertainment" />} />
-        <Route path="/general" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" />} />
-        <Route path="/health" element={<News setProgress={setProgress} key="health" pageSize={pageSize} country="us" category="health" />} />
-        <Route path="/science" element={<News setProgress={setProgress} key="science" pageSize={pageSize} country="us" category="science" />} />
-        <Route path="/sports" element={<News setProgress={setProgress} key="sports" pageSize={pageSize} country="us" category="sports" />} />
-        <Route path="/technology" element={<News setProgress={setProgress} key="technology" pageSize={pageSize} country="us" category="technology" />} />
+        <Route path="/news" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" />} />
+        <Route path="/news/business" element={<News setProgress={setProgress} key="business" pageSize={pageSize} country="us" category="business" />} />
+        <Route path="/news/entertainment" element={<News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="us" category="entertainment" />} />
+        <Route path="/news/general" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" />} />
+        <Route path="/news/health" element={<News setProgress={setProgress} key="health" pageSize={pageSize} country="us" category="health" />} />
+        <Route path="/news/science" element={<News setProgress={setProgress} key="science" pageSize={pageSize} country="us" category="science" />} />
+        <Route path="/news/sports" element={<News setProgress={setProgress} key="sports" pageSize={pageSize} country="us" category="sports" />} />
+        <Route path="/news/technology" element={<News setProgress={setProgress} key="technology" pageSize={pageSize} country="us" category="technology" />} />
       </Routes>
         </Router>
       </div>
