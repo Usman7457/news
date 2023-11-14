eba52c64df896bb6acb60eb9881feeae6cbed668
Usman7457 <usman.nxg@gmail.com>
2023-11-14 19:21:55 +0500

    changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -13,28 +13,28 @@ const NavBar = ()=> {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/news">Home</Link>
+          <Link className="nav-link" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/news/business">Business</Link>
+          <Link className="nav-link" to="/business">Business</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/news/entertainment">Entertainment</Link>
+          <Link className="nav-link" to="/entertainment">Entertainment</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/news/general">General</Link>
+          <Link className="nav-link" to="/general">General</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/news/health">Health</Link>
+          <Link className="nav-link" to="/health">Health</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/news/science">Science</Link>
+          <Link className="nav-link" to="/science">Science</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/news/sports">Sports</Link>
+          <Link className="nav-link" to="/sports">Sports</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/news/technology">Technology</Link>
+          <Link className="nav-link" to="/technology">Technology</Link>
         </li>
       </ul>
     </div>
